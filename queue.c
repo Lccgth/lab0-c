7efac737b291fd18ca35c66c97be165cf45db930
Lccgth <p76111254@gs.ncku.edu.tw>
2024-02-26 14:52:03 +0800

    Implement q_insert_tail()

--- queue.c
+++ queue.c
@@ -62,6 +62,21 @@ bool q_insert_head(struct list_head *head, char *s)
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head || !s)
+        return false;
+
+    element_t *element = malloc(sizeof(element_t));
+    if (!element)
+        return false;
+
+    element->value = strdup(s);
+    if (!element->value) {
+        free(element);
+        return false;
+    }
+
+    list_add_tail(&element->list, head);
+
     return true;
 }
 
